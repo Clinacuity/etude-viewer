9d0961aef7ba30fec2f1c96ba92e20c795b92a35
JK Accetta <jkaccetta@clinacuity.com>
2017-08-11 10:11:39 -0400

    DEV-22: added method to annotations to get metrics

--- src/main/java/com/clinacuity/acv/context/Annotations.java
+++ src/main/java/com/clinacuity/acv/context/Annotations.java
@@ -1,7 +1,7 @@
 package com.clinacuity.acv.context;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
@@ -13,9 +13,25 @@ import java.util.*;
 public class Annotations {
     private static final Logger logger = LogManager.getLogger();
 
+    private static final String METRICS_KEY = "metrics";
+    private static final String METRICS_TYPES_KEY = "by-type";
+    private static final String METRICS_AGGREGATE_KEY = "aggregate";
+    private static final String TRUE_POS_KEY = "TP";
+    private static final String FALSE_POS_KEY = "FP";
+    private static final String FALSE_NEG_KEY = "FN";
+    private static final String TRUE_NEG_KEY = "TN";
+    private static final String CONTENT_KEY = "raw_content";
+    private static final String OFFSETS_KEY = "offset_mapping";
+    private static final String ANNOTATIONS_KEY = "annotations";
+    private static final String ANNOTATION_TYPE_KEY = "type";
+
     private JsonObject root;
+    private JsonObject metrics = null;
+    private JsonObject offsets = null;
     private Map<String, List<JsonObject>> annotationMap = new HashMap<>();
     public Map<String, List<JsonObject>> getAnnotationMap() { return annotationMap; }
+    public boolean hasOffsetMapping() { return root.has(OFFSETS_KEY); }
+    public boolean hasMetrics() { return root.has(METRICS_KEY); }
 
     /**
      * Convenience object for managing a document's Json objects.
@@ -30,9 +46,15 @@ public class Annotations {
                 JsonParser parser = new JsonParser();
                 root = parser.parse(FileUtils.readFileToString(new File(filePath), "UTF-8")).getAsJsonObject();
 
-                // TODO: do something we these
-                root.get("offset_mapping").getAsJsonObject();
-                loadAnnotations(root.get("annotations").getAsJsonObject());
+                if (hasOffsetMapping()) {
+                    root.get(OFFSETS_KEY).getAsJsonObject();
+                }
+
+                if (hasMetrics()) {
+                    metrics = root.get(METRICS_KEY).getAsJsonObject();
+                }
+
+                loadAnnotations(root.get(ANNOTATIONS_KEY).getAsJsonObject());
             } catch (IOException e) {
                 logger.throwing(e);
             }
@@ -43,7 +65,7 @@ public class Annotations {
         annotationRoot.entrySet().forEach(entry -> {
             try {
                 JsonObject childAnnotation = entry.getValue().getAsJsonArray().get(0).getAsJsonObject();
-                String type = childAnnotation.get("type").getAsString();
+                String type = childAnnotation.get(ANNOTATION_TYPE_KEY).getAsString();
 
                 if (!annotationMap.keySet().contains(type)) {
                     annotationMap.put(type, new ArrayList<>());
@@ -62,6 +84,69 @@ public class Annotations {
     }
 
     public String getRawText() {
-        return root.get("raw_content").getAsString();
+        return root.get(CONTENT_KEY).getAsString();
+    }
+
+    /**
+     * Returns the True Positive percentage for any given Annotation type.
+     * @param annotationType    The Annotation whose metrics we are collecting
+     * @return                  The value of the metric for the given Annotation
+     */
+    public double getMetricsTruePositive(String annotationType) {
+        return getMetricsItem(annotationType, TRUE_POS_KEY);
+    }
+
+    /**
+     * Returns the False Positive percentage for any given Annotation type
+     * @param annotationType    The Annotation whose metrics we are collecting
+     * @return                  The value of the metric for the given Annotation
+     */
+    public double getMetricsFalsePositive(String annotationType) {
+        return getMetricsItem(annotationType, FALSE_POS_KEY);
+    }
+
+    /**
+     * Returns the False Negative percentage for any given Annotation type
+     * @param annotationType    The Annotation whose metrics we are collecting
+     * @return                  The value of the metric for the given Annotation
+     */
+    public double getMetricsFalseNegative(String annotationType) {
+        return getMetricsItem(annotationType, FALSE_NEG_KEY);
+    }
+
+    /**
+     * True negatives are impossible to determine; this value should always be 0.0d
+     * @param annotationType    The Annotation whose metrics we are collecting
+     * @return                  The value of the metric for the given Annotation
+     */
+    public double getMetricsTrueNegative(String annotationType) {
+        return getMetricsItem(annotationType, TRUE_NEG_KEY);
+    }
+
+    private double getMetricsItem(String annotationKey, String metricKey) {
+        JsonObject json;
+
+        if (metrics != null) {
+            if (metrics.has(METRICS_TYPES_KEY)) {
+                json = metrics.get(METRICS_TYPES_KEY).getAsJsonObject();
+                if (json.has(annotationKey)) {
+                    json = json.get(annotationKey).getAsJsonObject();
+                    if (json.has(metricKey)) {
+                        return json.get(metricKey).getAsDouble();
+                    } else {
+                        logger.warn("The metrics object has no information for value <{}> on key <{}>", metricKey, annotationKey);
+                    }
+                } else {
+                    logger.warn("The metrics object has no information for key <{}>", annotationKey);
+                    return 0.0d;
+                }
+            } else {
+                logger.throwing(new JsonParseException("Key <" + METRICS_TYPES_KEY + "> does not exist!"));
+                return 0.0d;
+            }
+        } else {
+            logger.warn("There is no metrics object; key <{}> begin set to 0.0d.");
+            return 0.0d;
+        }
     }
 }
