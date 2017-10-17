47e91fcefc5eeef5ae8cb6ef98b7f1e6563a16fc
JK Accetta <jkaccetta@clinacuity.com>
2017-10-17 15:39:02 -0400

    DEV-85: added NavBarButtons and style, populated NavBar

--- src/main/java/com/clinacuity/acv/controllers/NavBarController.java
+++ src/main/java/com/clinacuity/acv/controllers/NavBarController.java
@@ -1,5 +1,8 @@
 package com.clinacuity.acv.controllers;
 
+import com.clinacuity.acv.context.AcvContext;
+import com.clinacuity.acv.controls.NavBarButton;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,10 +10,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NavBarController implements Initializable {
-    private static final Logger logger = LogManager.getLogger();
+    @FXML private NavBarButton mainPageButton;
+    @FXML private NavBarButton etudeRunnerButton;
+    @FXML private NavBarButton loadScreenButton;
+    @FXML private NavBarButton configBuilderButton;
+    @FXML private NavBarButton compareViewerButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        mainPageButton.setOnMouseClicked(event -> AcvContext.getMainController().reloadContent(AcvContext.APP_MAIN_PAGE));
+        etudeRunnerButton.setOnMouseClicked(event -> AcvContext.getMainController().reloadContent(AcvContext.ETUDE_RUNNER));
+        loadScreenButton.setOnMouseClicked(event -> AcvContext.getMainController().reloadContent(AcvContext.LOAD_SCREEN));
+        configBuilderButton.setOnMouseClicked(event -> AcvContext.getMainController().reloadContent(AcvContext.CONFIGURATION_BUILDER));
+        compareViewerButton.setOnMouseClicked(event -> AcvContext.getMainController().reloadContent(AcvContext.COMPARISON_VIEW));
     }
 }
